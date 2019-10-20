3f177703c3c4cd87854fd6c207406bf634d464f7
Nadine Ju <nadine.jura@online.de>
2019-10-19 20:26:03 -0700

    javascript outlined

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+/******************************
+VARIABLES
+******************************/
+
+// store questions, answer options, and solutions
+
+// count correct answers
+
+// timer variables
+
+/******************************
+FUNCTIONS
+******************************/
+
+// display question
+
+// check if chosen answer is correct
+
+/******************************
+OBJECTS
+******************************/
+
+// timer
+
+/******************************
+GAME CODE
+******************************/
