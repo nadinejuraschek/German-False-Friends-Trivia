f8b8ea28e4555f3b2b32bb35a8672d6b28750ef1
Nadine Ju <nadine.jura@online.de>
2019-10-19 20:27:07 -0700

    questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,67 @@ VARIABLES
 ******************************/
 
 // store questions, answer options, and solutions
+var questions = [
+    {
+        question: "What does 'Es wird schon hell' mean?",
+        choices: ["It is slowly getting brighter.", "Oh hell, no!", "It will be hell"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What is 'Handy'?",
+        choices: ["A handyman", "Something is convenient", "A mobile phone"],
+        correctAnswer: 3
+    },
+    {
+        question: "What does 'bekommen' mean?",
+        choices: ["(to) get", "(to) begin", "(to) become"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What is a 'fatal' mistake?",
+        choices: ["A learning opportunity", "A deadly mistake", "An funny mishap"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is a 'Bürger'?",
+        choices: ["A delicious food item", "A citizen", "A castle or fortress"],
+        correctAnswer: 2
+    },
+
+    {
+        question: "What is the meaning of 'unterstehen'?",
+        choices: ["(to) stand under something", "(to) understand", "(to) be subordinate to"],
+        correctAnswer: 3
+    },
+    {
+        question: "What is a 'Tablette'?",
+        choices: ["A pill", "A tray", "A tablet"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What does a 'Chef' do?",
+        choices: ["Cooks meals", "Manages employees or a business", "Oversees the police department"],
+        correctAnswer: 2
+    },
+    {
+        question: "What is the meaning of 'gültig'?",
+        choices: ["Valid", "Honest", "Guilty"],
+        correctAnswer: 1
+    },
+
+    {
+        question: "What is a 'Gymnasium'?",
+        choices: ["A sneaker store", "A gym", "High School"],
+        correctAnswer: 3
+    }
+];
 
 // count correct answers
+var questionIndex = 0;
+var correctAnswers = 0;
 
 // timer variables
 
